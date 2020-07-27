3126791b034c0af7d7f4aa74ccf4896e3a7e5865
yyjlincoln <yyjlincoln@gmail.com>
2020-07-27 23:45:21 +1000

    Update style

--- PrintUI/AllDoc.js
+++ PrintUI/AllDoc.js
@@ -14,6 +14,7 @@ var app = new Vue({
                     for(var x=0;x<rst.length;x++){
                         rst[x].qr="https://apis.mcsrv.icu/qr?urlEncoded="+btoa("https://apis.mcsrv.icu/viewDocumentByID?docID="+rst[x].docID)
                         rst[x].dScanned = new Date(rst[x].dScanned*1000)
+                        rst[x].link = "https://apis.mcsrv.icu/viewDocumentByID?docID="+rst[x].docID
                     }
                     this.documents=rst
                 })
