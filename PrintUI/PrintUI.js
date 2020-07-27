814b5c440b5de296d5a22af9f3e6c466cba796a6
yyjlincoln <yyjlincoln@gmail.com>
2020-07-27 17:10:59 +1000

    Update PrintUI

--- PrintUI/PrintUI.js
+++ PrintUI/PrintUI.js
@@ -1,13 +1,47 @@
 var app = new Vue({
-    el:'#app',
-    data:{
+    el: '#app',
+    data: {
         documents: []
     },
-    methods:{
-        loadDocuments: function(){
+    methods: {
+        loadDocuments: function () {
             this.documents = [{
-                'title':'Test'
+                'name': 'Test Document',
+                'docID':'Document ID',
+                'subject':'Software Design and Development',
+                'status':'Digital (PDF)',
+                'dScanned':'2020/07/27',
+                'comments':'',
+                'desc':'There is no description available for this document.',
+                'qr':'https://www.google.com/logos/doodles/2020/jeanne-barets-280th-birthday-6753651837108693-l.png'
+            // }]
+            },{
+                'name': 'Test Document',
+                'docID':'Document ID',
+                'subject':'Software Design and Development',
+                'status':'Digital (PDF)',
+                'dScanned':'2020/07/27',
+                'comments':'',
+                'desc':'There is no description available for this document.',
+                'qr':'https://www.google.com/logos/doodles/2020/jeanne-barets-280th-birthday-6753651837108693-l.png'
+            },{
+                'name': 'Test Document',
+                'docID':'Document ID',
+                'subject':'Software Design and Development',
+                'status':'Digital (PDF)',
+                'dScanned':'2020/07/27',
+                'comments':'',
+                'desc':'There is no description available for this document.',
+                'qr':'https://www.google.com/logos/doodles/2020/jeanne-barets-280th-birthday-6753651837108693-l.png'
             }]
+        },
+        invokePrint: function(){
+            window.print()
         }
+    },
+    mounted: function () {
+        this.loadDocuments()
+        window.print()
+        // window.close()
     }
 })
