6921efe6bd4ffc75eaf0aede7d73564a05a8cd94
yyjlincoln <yyjlincoln@gmail.com>
2020-07-28 15:32:31 +1000

    Update

---
+++ GmailIntegration/config.py
@@ -0,0 +1,6 @@
+# Configuration file template:
+# Note that this password will be changed frequently.
+Config = {
+    'email.email':'automated.yan@gmail.com',
+    'email.password':'XgS7jxM3gaP13aVUhXZ5Rg=='
+}
--- GmailIntegration/server.py
+++ GmailIntegration/server.py
@@ -1,32 +1,33 @@
 import zmail
 import time
 import requests
+import io
+from werkzeug.utils import secure_filename
+from config import Config
 
-server = zmail.server('automated.yan@gmail.com','XgS7jxM3gaP13aVUhXZ5Rg==')
+server = zmail.server(Config['email.email'],Config['email.password'])
 
 # server.get_mails()
 
 while True:
     try:
         d = server.get_latest()
-        if len(d['attachments'])==1:
+        if len(d['attachments']) == 1:
             fileName, fData = d['attachments'][0]
             subject = d['subject']
-            # POST
-            print({
-                'name':subject,
-                'subject':'Unspecified',
-                'comments':'This record is automatically created by DocumentX Gmail Integration.'
+            # Create a Stream
+            fStream = io.BytesIO(fData)
+            fStream.name=fileName
+            # POST to the server
+            r = requests.post('https://apis.mcsrv.icu/uploadDocument', data={
+                'name': subject,
+                'subject': 'Unspecified',
+                'comments': 'This record is automatically created by DocumentX Gmail Integration.'
+            }, files={
+                'file': fStream
             })
-            requests.post('https://apis.mcsrv.icu/upload',{
-                'name':subject,
-                'subject':'Unspecified',
-                'comments':'This record is automatically created by DocumentX Gmail Integration.'
-            }, files = {
-                fileName: fData
-            })
-        time.sleep(30)
-    except:
+            print(r.json())
+    except Exception as e:
         print('Could not get email.')
+    finally:
         time.sleep(30)
-        continue
