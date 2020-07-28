0f0a8d6b65236c6f1a25703fd8add19ef7d7d0cd
yyjlincoln <yyjlincoln@gmail.com>
2020-07-28 15:54:59 +1000

    Update send receipt

--- GmailIntegration/config.py
+++ GmailIntegration/config.py
@@ -2,5 +2,6 @@
 # Note that this password will be changed frequently.
 Config = {
     'email.email':'automated.yan@gmail.com',
-    'email.password':'XgS7jxM3gaP13aVUhXZ5Rg=='
+    'email.password':'XgS7jxM3gaP13aVUhXZ5Rg==',
+    'email.reciept':['yijun.yan@education.nsw.gov.au']
 }
--- GmailIntegration/server.py
+++ GmailIntegration/server.py
@@ -4,6 +4,7 @@ import requests
 import io
 from werkzeug.utils import secure_filename
 from config import Config
+import base64
 
 server = zmail.server(Config['email.email'],Config['email.password'])
 
@@ -25,8 +26,32 @@ while True:
                 'comments': 'This record is automatically created by DocumentX Gmail Integration.'
             }, files={
                 'file': fStream
-            })
-            print(r.json())
+            }) 
+
+            status = 'Failed'
+            DocID = 'unknown'
+            try:
+                r = r.json()
+                status = r['message'] + ' ('+str(r['code'])+')'
+                DocID = r['docID']
+            except Exception as e:
+                status = 'Failed ('+ str(e) + ')'
+            
+            htmlContent = f'''
+            <p><b>File {fileName} has been scanned.</b></p>
+            <p>{status}</p>
+            <p></p>
+            <p>DocID: {DocID}</p>
+            <p>Link: <a href="https://apis.mcsrv.icu/viewDocumentByID?docID={DocID}">here</a></p>
+            <img src="https://apis.mcsrv.icu/qr?urlEncoded={base64.b64encode(str(f'https://apis.mcsrv.icu/viewDocumentByID?docID={DocID}').encode()).decode()}"></img>
+            <p></p>
+            <p>This is an automated email from DocumentX-GmailIntegration.</p>
+            '''
+            try:
+                server.send_mail(Config['email.reciept'],htmlContent)
+            except Exception as e:
+                print('Could not send email reciept: '+str(e))
+
     except Exception as e:
         print('Could not get email.')
     finally:
