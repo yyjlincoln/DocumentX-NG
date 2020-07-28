292d8a38163db05a767a5aa6cbdcbc64cc18e022
yyjlincoln <yyjlincoln@gmail.com>
2020-07-28 14:31:23 +1000

    Add integration

---
+++ GmailIntegration/server.py
@@ -0,0 +1,32 @@
+import zmail
+import time
+import requests
+
+server = zmail.server('automated.yan@gmail.com','XgS7jxM3gaP13aVUhXZ5Rg==')
+
+# server.get_mails()
+
+while True:
+    try:
+        d = server.get_latest()
+        if len(d['attachments'])==1:
+            fileName, fData = d['attachments'][0]
+            subject = d['subject']
+            # POST
+            print({
+                'name':subject,
+                'subject':'Unspecified',
+                'comments':'This record is automatically created by DocumentX Gmail Integration.'
+            })
+            requests.post('https://apis.mcsrv.icu/upload',{
+                'name':subject,
+                'subject':'Unspecified',
+                'comments':'This record is automatically created by DocumentX Gmail Integration.'
+            }, files = {
+                fileName: fData
+            })
+        time.sleep(30)
+    except:
+        print('Could not get email.')
+        time.sleep(30)
+        continue
