ef8758483a1ce190bc7b12f48eb197aff77b5d75
Lincoln Yan <yyjlincoln@gmail.com>
2021-10-06 01:14:46 +1100

    Update: Now uses cache system

--- filestore.py
+++ filestore.py
@@ -1,12 +1,29 @@
 from s3gateway import gateway, Gateway
 import os
+import threading
+import time
 
 gateway: Gateway
 
 CACHE_PATH = '/tmp/documentx'
 
+
+def uploadFileFromCache(docID):
+    with open(os.path.join(CACHE_PATH, docID), 'rb') as f:
+        if gateway.uploadFile(docID, f.read()):
+            os.remove(os.path.join(CACHE_PATH, docID + '.uploading'))
+        else:
+            with open(os.path.join(CACHE_PATH, docID + '.uploadfailure'), 'w') as f:
+                f.write(str(time.time()))
+
 def saveFile(docID, content):
-    gateway.uploadFile(docID, content)
+    with open(os.path.join(CACHE_PATH, docID), 'wb') as f:
+        f.write(content)
+    with open(os.path.join(CACHE_PATH, docID + '.uploading'), 'w') as f:
+        f.write(str(time.time()))
+    t = threading.Thread(target=uploadFileFromCache, args=(docID,))
+    t.start()
+
 
 def renameFile(docID, newID) -> bool:
     # oldPath = getStorageLocation(docID)
@@ -18,11 +35,14 @@ def renameFile(docID, newID) -> bool:
     #     return False
     raise Exception("Not implemented")
 
+
 def deleteFile(docID):
     gateway.deleteFile(docID)
 
+
 def getFileLink(docID):
     return gateway.getURL(docID, expiresIn=30)
+
 
 def getStorageLocation(docID):
     path = os.path.join(CACHE_PATH, docID)
